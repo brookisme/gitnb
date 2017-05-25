732958c73b31f72edcdf505cab1b03f60fd2cc03
Brookie Guzder-Williams <bguzder-williams@wri.org>
2017-05-25 13:41:57 -0700

    notebook_list, rglob

--- nbgit/__init__.py
+++ nbgit/__init__.py
@@ -1,1 +1,16 @@
 # __init__.py
+import os
+import fnmatch
+
+def notebook_list():
+    return rglob('*.ipynb')
+
+
+def rglob(match='*',root='.'):
+    """ Recursive Glob
+    """
+    matches = []
+    for froot, _, filenames in os.walk(root):
+      for filename in fnmatch.filter(filenames, match):
+          matches.append(os.path.join(froot, filename))
+    return matches
