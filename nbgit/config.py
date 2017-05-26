5dcc4a6cb1dc1e606da07e3626a4eddd1c3d554d
Brookie Guzder-Williams <bguzder-williams@wri.org>
2017-05-26 12:24:37 -0700

    temp fix to user config bug

--- nbgit/config.py
+++ nbgit/config.py
@@ -1,8 +1,11 @@
 import os
+import sys
 CONFIG_PATH='./nbgit_config.py'
 
 
 if os.path.isfile(CONFIG_PATH):
+    print "PROJECT DIRECTORY ADDED TO SYS PATH!"
+    sys.path.append('./')
     from nbgit_config import *
 else:
     from _default_config import *
