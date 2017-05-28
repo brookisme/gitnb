f1364af1a8a65ebcf9864e7ea1e67bf34b3d2a61
Brookie Guzder-Williams <bguzder-williams@wri.org>
2017-05-28 13:51:38 -0700

    Py2NB(path) not Py2NB(lines).  doc-comments

--- nbgit/constants.py
+++ nbgit/constants.py
@@ -5,9 +5,21 @@
 #
 ###########################################################
 
+""" FIXED IPYNB KEYS
+"""
+TYPE_KEY='cell_type'
+OUTPUTS_KEY='outputs'
+OUTPUT_KEY='text'
+SOURCE_KEY='source'
+CELLS_KEY='cells'
+CODE_TYPE='code'
+MARKDOWN_TYPE='code'
+RAW_TYPE='code'
+
+
 # """ START/END of code block
 # """
-CODE_START='"""[code]"""'
+CODE_START='"""[{}]"""'.format(CODE_TYPE)
 CODE_END='""""""'
 
 """ START/END of markdown|raw block 
@@ -15,20 +27,11 @@ CODE_END='""""""'
 """
 UNCODE_START='"""[{}]'
 UNCODE_END='"""'
-MARKDOWN_START=UNCODE_START.format('markdown')
-RAW_START=UNCODE_START.format('raw')
+MARKDOWN_START=UNCODE_START.format(MARKDOWN_TYPE)
+RAW_START=UNCODE_START.format(RAW_TYPE)
 
 """ START/END of ouputs block
 """
 OUTPUTS_START='"""[output]'
 OUTPUTS_END='"""'
 
-
-""" FIXED IPYNB KEYS
-"""
-TYPE_KEY='cell_type'
-OUTPUTS_KEY='outputs'
-OUTPUT_KEY='text'
-SOURCE_KEY='source'
-CODE_TYPE='code'
-CELLS_KEY='cells'
--- nbgit/tonb.py
+++ nbgit/tonb.py
@@ -17,8 +17,8 @@ class Py2NB(object):
     cell_type=None
     ipynb_dict={}
     
-    def __init__(self,lines):
-        self.lines=lines
+    def __init__(self,path):
+        self.lines=self._read_py_file(path)
         for line in lines:
             self._process_line(line)
         self.ipynb_dict['cells']=self.cells
@@ -26,6 +26,8 @@ class Py2NB(object):
 
 
     def write(self,fname):
+        """ Write IPYNB JSON to file
+        """
         ipynb_json=json.dumps(
             self.ipynb_dict,
             sort_keys=True,
@@ -38,6 +40,13 @@ class Py2NB(object):
     # INTERNAL: IPYNB_DICT CONSTRUCTION
     #
     def _process_line(self,line):
+        """ Process Line from file
+            - if new cell: init cell
+            - if end cell: 
+                - close cell
+                - append cell to cells
+            - if cell-line: add cell-line to cell
+        """
         line=self._clean(line)
         cell_type=self._is_new_cell(line)
         if cell_type: self._init_cell(cell_type)
@@ -50,17 +59,24 @@ class Py2NB(object):
 
 
     def _is_new_cell(self,line):
+        """ Check if new cell
+            if new cell return cell-type
+            else return None
+        """
         if line==CODE_START:
-            return "code"
+            return CODE_TYPE
         elif line==MARKDOWN_START:
-            return "markdown"
+            return MARKDOWN_TYPE
         elif line==RAW_START:
-            return "raw"
+            return RAW_TYPE
         else:
             return None
 
 
     def _is_end_of_cell(self,line):
+        """ Check if end of cell
+            return bool
+        """
         if self.cell_type==CODE_TYPE:
             return line==CODE_END
         else:
@@ -68,6 +84,8 @@ class Py2NB(object):
 
 
     def _init_cell(self,cell_type):
+        """ Initialize New Cell
+        """
         self.cell_type=cell_type
         self.cell=self._new_cell(cell_type)
         if cell_type=='code':
@@ -76,10 +94,16 @@ class Py2NB(object):
 
 
     def _insert_line(self,line):
+        """ Insert Source line into cell-source
+        """
         self.cell['source'].append("{}\n".format(line))
 
 
     def _close_cell(self):
+        """ Close cell
+            - remove '\n' from last cell-source-line
+            - set cell/cell_type to None
+        """        
         lastline=self.cell['source'].pop()
         self.cell['source'].append(lastline.rstrip('\n'))
         self.cells.append(self.cell)
@@ -88,6 +112,8 @@ class Py2NB(object):
 
 
     def _new_cell(self,cell_type):
+        """ Return New Cell
+        """    
         return {
             "cell_type": cell_type,
             "metadata": {},
@@ -95,6 +121,8 @@ class Py2NB(object):
 
 
     def _meta(self):
+        """ Return Notebook Metadata
+        """   
         major,minor,micro,_,_=sys.version_info
         return {
         "metadata": {
@@ -115,15 +143,25 @@ class Py2NB(object):
             "nbformat": 4,
             "nbformat_minor": 2}
 
+
     #
     # UTILS
     #
-    def _clean(self,s):
-        return s.rstrip('\n').rstrip(' ')
+    def _read_py_file(self,path):
+        """ Read file
+            return lines list
+        """ 
+        with open(input_path,'r') as pyfile:
+            lines=pyfile.readlines()
+        return lines
 
 
-    def _eq(self,s1,s2):
-        return s1==s2
+    def _clean(self,line):
+        """ Remove spaces and line break 
+            from end of line
+        """   
+        return line.rstrip('\n').rstrip(' ')
+
 
 
 
