c4100001fe4067c2ea2f266970e08723a8ae29ab
Brookie Guzder-Williams <bguzder-williams@wri.org>
2017-05-28 15:32:08 -0700

    version 0.0.0.2: attempting to include yaml

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from distutils.core import setup
 setup(
   name = 'nbgit',
   packages = ['nbgit'],
-  version = '0.0.0.1',
+  version = '0.0.0.2',
   description = 'Lanuches and manages F250 biweekly/daily runs',
   author = 'Brookie Guzder-Williams',
   author_email = 'brook.williams@gmail.com',
   url = 'https://github.com/brookisme/nbgit',
   download_url = 'https://github.com/brookisme/nbgit/tarball/0.1',
   keywords = ['ipython', 'notebook','git'],
+  data_files=[('config',['nbgit/default.config.yaml'])],
   classifiers = [],
   entry_points={
       'console_scripts': [
