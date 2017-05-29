d23660dbd89e65a9db21f2a8c666d863cbf89661
Brookie Guzder-Williams <bguzder-williams@wri.org>
2017-05-28 20:16:39 -0700

    include_package_data

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from distutils.core import setup
 setup(
   name = 'nb_git',
   packages = ['nb_git'],
-  version = '0.0.0.1',
+  version = '0.0.0.2',
   description = 'Git Tracking for Python Notebooks',
   author = 'Brookie Guzder-Williams',
   author_email = 'brook.williams@gmail.com',
   url = 'https://github.com/brookisme/nb_git',
   download_url = 'https://github.com/brookisme/nb_git/tarball/0.1',
   keywords = ['ipython', 'notebook','git'],
+  include_package_data=True,
   data_files=[('config',['nb_git/default.config.yaml'])],
   classifiers = [],
   entry_points={
