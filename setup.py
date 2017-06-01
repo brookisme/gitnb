99417bf5d95ad2d4409ba27941d3e79b82b7777b
Brookie Guzder-Williams <bguzder-williams@wri.org>
2017-06-01 15:06:46 -0700

    version 0.0.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'gitnb',
   packages = ['gitnb'],
-  version = '0.0.1.3',
+  version = '0.0.1.4',
   description = 'Git Tracking for Python Notebooks',
   author = 'Brookie Guzder-Williams',
   author_email = 'brook.williams@gmail.com',
